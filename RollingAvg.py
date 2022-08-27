21dc46965636b4531431e51212c34962f8d7b656
christophertesar <ctesar@sfu.ca>
2022-08-27 11:36:49 -0700

    poopy

--- RollingAvg.py
+++ RollingAvg.py
@@ -1,25 +1,36 @@
-import requests
+import sys
+import threading
+from os import path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from Package.APIResources import APIResources
 import ItemID
 
-url_latest = "https://prices.runescape.wiki/api/v1/osrs/latest"
-url_1h = "https://prices.runescape.wiki/api/v1/osrs/1h"
-url_m = "https://prices.runescape.wiki/api/v1/osrs/10m"
-url_24h = "https://prices.runescape.wiki/api/v1/osrs/24h"
 
-headers = {
-    "User-Agent": "dump watcher - @Roflnator#3778"
-}
+def main():
+    headers = {
+        "User-Agent": "dump watcher - @Roflnator#3778"
+    }
 
-response_latest = requests.get(url_latest, headers=headers)
-response_latest = response_latest.json()
+    api = APIResources(headers=headers)
+    sample = []
+    while True:
+        reply = api.getLatest()
+        sample.append(reply)
+        if len(sample) < 10:
+            print("guh")
 
-response_1h = requests.get(url_1h, headers=headers)
-response_1h = response_1h.json()
 
-response_m = requests.get(url_m, headers=headers)
-response_m = response_m.json()
 
-response_24h = requests.get(url_m, headers=headers)
-response_24h = response_24h.json()
 
-print(response_m)
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
