e3f8b5e3a82da27e65e8303210ddee1d4877b3cf
Elwin Shen <eshen1998@gmail.com>
2023-01-06 00:07:27 -0800

    Detects items now

--- ItemID.py
+++ ItemID.py
@@ -8,67 +8,6 @@ headers = {
 }
 
 
-class ItemQueues(dict):
-    """
-    class to store collection of queues for item prices
-    """
-    MAX_LEN_STORED = 10
-
-    def __init__(self):
-        super().__init__()
-        self._initializeQueues()
-        self.updateQueues()
-
-    def _initializeQueues(self):
-        """
-        only creates queues for items with data from past 24h
-        """
-        api = APIResources(headers=headers)
-        response = api.get24h()
-        for item in response["data"]:
-            high_price = response["data"][item]["avgHighPrice"]
-            low_price = response["data"][item]["avgLowPrice"]
-            if high_price is None or low_price is None:
-                continue
-            else:
-                self[item] = {"highPrices": deque([high_price], maxlen=self.MAX_LEN_STORED),
-                              "lowPrices": deque([low_price], maxlen=self.MAX_LEN_STORED),
-                              "highRolling": 0,
-                              "lowRolling": 0}
-
-    def updateQueues(self):
-        """
-        updates queues in self
-        """
-        api = APIResources(headers=headers)
-        response = api.getLatest()
-        for id, prices in self.items():
-            high = response["data"][id]["high"]
-            low = response["data"][id]["high"]
-            prices["highPrices"].append(high)
-            prices["lowPrices"].append(low)
-
-    def updateRolling(self):
-        def f(idx, price, denominator): return round(price * (idx / denominator))
-        for id, queue in self.items():
-            # highPrices and lowPrices should always be the same length
-            denominator = sum(range(1, len(queue["highPrices"]) + 1))
-            length = len(queue["highPrices"])
-            high_prices_weighted = [f(i, high_price, denominator) for i, high_price in enumerate(queue['highPrices'], start=1)]
-            low_prices_weighted = [f(i, low_price, denominator) for i, low_price in enumerate(queue['lowPrices'], start=1)]
-            high_rolling_average = sum(high_prices_weighted)
-            low_rolling_average = sum(low_prices_weighted)
-            self[id]["highRolling"] = high_rolling_average
-            self[id]["lowRolling"] = low_rolling_average
-
-
-    def findStdDev(self):
-        ans = {}
-        for id, queue in self.items():
-            ans[id] = np.std(queue)
-        return ans
-
-
 class ItemCollection(dict):
     """
     class to store info and pricing of an item
@@ -182,3 +121,128 @@ class ItemMap(dict):
 
     def idLookup(self, id: str) -> ItemInfo:
         return self[id]
+
+
+class ItemQueues(dict):
+    """
+    class to store collection of queues for item prices
+    """
+    MAX_LEN_STORED = 10
+    DATA = "data"
+    AVG_HIGH_PRICE = "avgHighPrice"
+    AVG_LOW_PRICE = "avgLowPrice"
+    HIGH_VOLUME = 10000
+    ROI = 5
+    POTENTIAL = 500000
+
+    def __init__(self, item_collection: ItemCollection):
+        super().__init__()
+        self.item_collection = item_collection
+        self._initializeQueues()
+        self.updateQueues()
+        self.updateRolling()
+
+    def _initializeQueues(self):
+        """
+        only creates queues for items with data from past 24h
+        """
+        api = APIResources(headers=headers)
+        response = api.get24h()
+        for item in response[self.DATA]:
+            high_price = response[self.DATA][item][self.AVG_HIGH_PRICE]
+            low_price = response[self.DATA][item][self.AVG_LOW_PRICE]
+            if high_price is None or low_price is None:
+                continue
+            else:
+                self[item] = {"highPrices": deque([high_price], maxlen=self.MAX_LEN_STORED),
+                              "lowPrices": deque([low_price], maxlen=self.MAX_LEN_STORED),
+                              "highRolling": 0,
+                              "lowRolling": 0,
+                              "highTime": 0,
+                              "lowTime": 0}
+
+    def updateQueues(self):
+        """
+        updates queues in self
+        """
+        api = APIResources(headers=headers)
+        response = api.getLatest()
+        for id, prices in self.items():
+            high = response[self.DATA][id]["high"]
+            low = response[self.DATA][id]["low"]
+            prices["highPrices"].append(high)
+            prices["lowPrices"].append(low)
+            prices["highTime"] = response[self.DATA][id]["highTime"]
+            prices["lowTime"] = response[self.DATA][id]["lowTime"]
+
+    def updateRolling(self):
+        def f(idx, price, denominator): return round(price * (idx / denominator))
+        for id, queue in self.items():
+            # highPrices and lowPrices should always be the same length
+            denominator = sum(range(1, len(queue["highPrices"]) + 1))
+            high_prices_weighted = [f(i, high_price, denominator) for i, high_price in enumerate(queue['highPrices'], start=1)]
+            low_prices_weighted = [f(i, low_price, denominator) for i, low_price in enumerate(queue['lowPrices'], start=1)]
+            high_rolling_average = sum(high_prices_weighted)
+            low_rolling_average = sum(low_prices_weighted)
+            self[id]["highRolling"] = high_rolling_average
+            self[id]["lowRolling"] = low_rolling_average
+
+    def findStdDev(self):
+        ans = {}
+        for id, queue in self.items():
+            ans[id] = np.std(queue)
+        return ans
+
+    def _filter(self, volume, potential, roi, limit, margin):
+        if volume is None or limit is None:
+            return False
+        if roi < self.ROI and potential < self.POTENTIAL:
+            return False
+        elif volume >= self.HIGH_VOLUME and (volume * 0.01) >= limit:
+            return True
+        elif volume < self.HIGH_VOLUME and (volume * 0.05 * margin) >= self.POTENTIAL:
+            return True
+        else:
+            return False
+
+    def dump_checker(self):
+        """
+        compare rolling and latest to find large differences
+        """
+        buy_dumps = []
+        sell_dumps = []
+        for id, prices in self.items():
+            volume, limit, name = (
+                self.item_collection[id].item_pricing.volume,
+                self.item_collection[id].item_info.limit,
+                self.item_collection[id].item_info.name
+            )
+            low_latest, high_latest = prices["lowPrices"][-1], prices["highPrices"][-1]
+
+            if high_latest < prices["highRolling"]:
+                margin = prices["highRolling"] - high_latest
+                potential = limit * margin
+                # Compares most recent buy price to rolling buy price and finds percent difference
+                roi = round((prices["highRolling"] - high_latest) / prices["highRolling"] * 100)
+                if self._filter(volume, potential, roi, limit, margin):
+                    buy_dumps.append([name, prices["highRolling"], high_latest, volume, limit, potential])
+            elif low_latest < prices["lowRolling"]:
+                margin = prices["highRolling"] - low_latest
+                potential = limit * margin
+                # Compares most recent sell price to rolling buy price and finds percentage difference
+                roi = round((prices["highRolling"] - low_latest) / prices["highRolling"] * 100)
+                if self._filter(volume, potential, roi, limit, margin):
+                    sell_dumps.append([name, prices["highRolling"], low_latest, volume, limit, potential])
+            else:
+                continue
+        buy_dumps.sort(key=lambda x: x[-1], reverse=True)
+        sell_dumps.sort(key=lambda x: x[-1], reverse=True)
+        print("buys")
+        for item in buy_dumps[:10]:
+            print(item)
+        print("sells")
+        for item in sell_dumps[:10]:
+            print(item)
+
+    def spikes(self):
+        raise NotImplemented
--- RollingAvg.py
+++ RollingAvg.py
@@ -2,59 +2,10 @@ import sys
 import threading
 import time
 from os import path
+import ItemID
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from APIResources import APIResources
 
-def main():
-    headers = {
-        "User-Agent": "dump watcher - @Roflnator#3778"
-    }
-
-    api = APIResources(headers=headers)
-    samples = []
-    while True:
-        reply = api.getLatest()
-        samples.append(reply)
-        if len(samples) < 10:
-            pass
-        elif len(samples) == 61:
-            samples.pop()
-        processing_thread = threading.Thread(target=process, args=samples)
-        processing_thread.start()
-        time.sleep(60)
-        processing_thread.join()
-
-
-def process(samples):
-    for sample in samples:
-        pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
+collection = ItemID.ItemCollection()
+queue = ItemID.ItemQueues(collection)
+queue.dump_checker()
